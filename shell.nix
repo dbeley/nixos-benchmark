5de064f89f54a246116ca890f4229e5cc38f7809
David BELEY <6568955+dbeley@users.noreply.github.com>
2025-07-30 12:40:07 +0000

    Include pip and pyyaml in shell

--- shell.nix
+++ shell.nix
@@ -4,6 +4,7 @@ pkgs.mkShell {
   buildInputs = with pkgs; [
     phoronix-test-suite
     bison flex gmp libaio SDL2 zlib openssl
-    python3 php nginx
+    (python3Full.withPackages (ps: with ps; [ pip pyyaml ]))
+    php nginx
   ];
 }
